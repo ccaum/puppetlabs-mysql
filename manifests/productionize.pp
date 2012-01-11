dc4ae63178e33345f1af6e0fcfa42dd38cb30e9b
Justin Ellison <justin.ellison@buckle.com>
2012-01-11 11:05:06 -0600

    remove test db grants

--- manifests/productionize.pp
+++ manifests/productionize.pp
@@ -2,4 +2,10 @@ class mysql::productionize {
   database { "test":
     ensure => absent,
   }
+  database_grant { "''@'%'/test":
+    ensure => absent,
+  }
+  database_grant { "''@'%'/test\_%":
+    ensure => absent,
+  }
 }
